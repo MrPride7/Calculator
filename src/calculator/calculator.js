2c157be96ab90c228d3bb8f51dce70a1a5212d12
Pride <serafim0604@gmail.com>
2019-08-18 13:32:15 +0800

    small omptimization

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -2,27 +2,31 @@
 
 const viewer = document.getElementById("viewer"); //viewer of calculator
 const allBtns = document.querySelectorAll(
-    "div.numbers > button.btn, div.actions > button.btn"); //all buttons of calculator
+    "#calculator button.btn not(#equation)"); //all buttons of calculator
 let outData;
 let inputData = [];
-
-allBtns.forEach(function (btn){
-    btn.addEventListener("click", ()=>{
-        // first of all there need to check two special situations(buttons):
-        // "c" - delete all, "ce" - delete last character
-        if(btn.innerHTML === "c") { // if button = "c", viewer output = 0
-            viewer.innerHTML = "0";
-        } else if(btn.innerHTML === "ce"){ // if button = "ce",
-            if(viewer.innerHTML.length === 1) { //and length of output = 1(
-                // it doesn't matter what character it is)
-                viewer.innerHTML = "0"; // we change output to 0
-            } else { // if button = "ce", but length != 1, we delete unnecessary part
-                viewer.innerHTML = viewer.innerHTML.slice(0,-1);
-            }
+console.log(allBtns);
+function getData() {
+    // first of all there need to check two special situations(buttons):
+    // "c" - delete all, "ce" - delete last character
+    if(this.innerHTML === "c") { // if button = "c", viewer output = 0
+        viewer.innerHTML = "0";
+    } else if(this.innerHTML === "ce"){ // if button = "ce",
+        if(viewer.innerHTML.length === 1) { //and length of output = 1(
+            // it doesn't matter what character it is)
+            viewer.innerHTML = "0"; // we change output to 0
+        } else { // if button = "ce", but length != 1, we delete unnecessary part
+            viewer.innerHTML = viewer.innerHTML.slice(0,-1);
+        }
         //if we know that button != "c" and button != "ce", we can write other characters
-        } else if(viewer.innerHTML === "0") {
-            viewer.innerHTML = btn.innerHTML;
-        } else viewer.innerHTML += btn.innerHTML;
-    })
+    } else if(viewer.innerHTML === "0") {
+        viewer.innerHTML = this.innerHTML;
+    } else viewer.innerHTML += this.innerHTML;
+}
+allBtns.forEach(function (btn){
+    btn.addEventListener("click", getData)
 });
+
+const equation = document.getElementById("equation");
+
 
