0c34f0a17d41a03d99c910a3885013e5fada363d
Pride <serafim0604@gmail.com>
2019-08-21 16:52:18 +0800

    half finished calculation

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -3,20 +3,21 @@
 const viewer = document.getElementById("viewer"); //viewer of calculator
 const allBtns = document.querySelectorAll(
     "#calculator button.btn:not(#equation)"); //all buttons of calculator
+const equation = document.getElementById("equation");
 let outData = [];
-let inputData = [];
+
 
 function getData() {
     // first of all there need to check two special situations(buttons):
     // "c" - delete all, "ce" - delete last character
-    if(this.innerHTML === "c") { // if button = "c", viewer output = 0
-        outData = [0];
+    if(this.innerHTML === "c") { // if button = "c", viewer output = 0(clear array)
+        outData = [];
     } else if(this.innerHTML === "ce"){ // if button = "ce",
         if(outData.length === 1) { //and length of output = 1(
             // it doesn't matter what character it is)
-            // we change output to 0
-            outData = [0];
-        } else { // if button = "ce", but length != 1, we delete unnecessary part
+            // we change output to 0(clear array)
+            outData = [];
+        } else { // if button = "ce", but length != 1, we delete last character
             outData.shift();
         }
         //if we know that button != "c" and button != "ce", we can write other characters
@@ -26,12 +27,32 @@ function getData() {
     } else {
         outData.push(this.innerHTML);
     }
-    viewer.innerHTML = outData.join("");
+    if (outData.length !== 0) {
+        viewer.innerHTML = outData.join(""); // if we have smth, out it
+    } else {
+        viewer.innerHTML = "0"; // if not, out "0"
+    }
+
 }
 allBtns.forEach(function (btn){
     btn.addEventListener("click", getData);
 });
 
-const equation = document.getElementById("equation");
-
+equation.addEventListener("click", function () {
+    let actions = []; // arr for actions
+    for (let i = 0; i < outData.length; i++) {
+        if (outData[i] === "*" || outData[i] === "/" || outData[i] === "+" || outData[i] === "-") {
+            actions.push(outData[i]);
+            outData[i] = " ";
+        }
+    }
+    outData = outData.join("").split(" "); //join characters to numbers
+    for (let item of outData){
+        if (item = " "){
+            viewer.innerHTML = "EROR"; // 2 actions in a row
+        }
+    }
+    console.log(outData);
+    console.log(actions);
+});
 
